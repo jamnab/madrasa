e62988fa21c038fa17b432775288694e0af28663
jamnab <jamesbmcnab@gmail.com>
2014-08-26 18:36:14 -0400

    added link & submit tracking

--- js/initial.js
+++ js/initial.js
@@ -64,7 +64,7 @@ function loadInitial() {
                     $('#title').append("Welcome, Here are some featured posts.");
             
                     $.each(data.results, function () {
-                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><div id='featured'><span>" + this.category + "</span></div><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div");
+                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' onclick='_gaq.push(['_trackEvent', 'External Link', 'Click', '" + this.title + "']);' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><div id='featured'><span>" + this.category + "</span></div><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div");
                     });
                     $('img').unveil(200);
                 } else {
@@ -118,7 +118,7 @@ function loadFeatures(featureID) {
                 if (data.results.length > 0) {
             
                     $.each(data.results, function () {
-                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div");
+                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' onclick='_gaq.push(['_trackEvent', 'External Link', 'Click', '" + this.title + "']);' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div");
                     });
                     $('img').unveil(200);
                 } else {
@@ -157,9 +157,9 @@ function doSearch() {
 					$('#link-display').append("<h1 id='title'>Here are How-Tos for <a href='#'>" + searchText + "</a></h1>");
 
 					$.each(data.results, function () {
-                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div>");
+                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' onclick='_gaq.push(['_trackEvent', 'External Link', 'Click', '" + this.title + "']);' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div>");
 					});
-					$("img").unveil(200);
+					$('img').unveil(200);
 
 				} else {
 					$('#link-display').append("<h1 id='title'>Sorry no results for <a href=''>" + searchText + "</a>. Here's some other posts.</h1>");
@@ -174,15 +174,18 @@ function doSearch() {
 	});
 }
 function submitAppear() {
-    $('#submit-panel').append('<div id="submit-display"><a id="close" href="#"><img src="img/close-btn-w.png"></a><form><h1>Submit a Post</h1><p>Share educational articles, videos, podcasts, infographics, and more.</p><input class="submit title" name="title" maxlength="75" type="text" placeholder="Enter title*" required autofocus><input class="submit link" name="link" maxlength="155" type="text" placeholder="http://example.com*" required><input class="submit image" name="image" type="text" maxlength="155" placeholder="Add image url (Optional)"><textarea class="submit desc" name="desc" type="text" rows="3" maxlength="155" placeholder="Enter a description*" required></textarea><select class="submit cat" name="category"><option class="category" value="" selected disabled style="display:none;">Choose a category</option><option class="category" value="Advice">Advice</option><option class="category" value="Design">Design</option><option class="category" value="Funding">Funding</option><option class="category" value="Growth">Growth</option><option class="category" value="Product">Product</option><option class="category" value="Sales">Sales</option><option class="category" value="Strategy">Strategy</option><option class="category" value="Team">Team</option><option class="category" value="Tech">Tech</option><option class="category" value="Users">Users</option><option class="category" value="Venture">Venture</option></select><input class="submit tags" name="tags" type="text" maxlength="75" placeholder="Separate tags with a space"><hr><input id="submit-btn" type="button" value="Submit"></div>');
+    $('#submit-panel').append("<div id='submit-display'><a id='close' href='#'><img src='img/close-btn-w.png'></a><form><h1>Submit a Post</h1><p>Share educational articles, videos, podcasts, infographics, and more.</p><input class='submit title' name='title' maxlength='75' type='text' placeholder='Enter title*' required autofocus><input class='submit link' name='link' maxlength='155' type='text' placeholder='http://example.com*' required><input class='submit image' name='image' type='text' maxlength='155' placeholder='Add image url (Optional)'><textarea class='submit desc' name='desc' type='text' rows='3' maxlength='155' placeholder='Enter a description*' required></textarea><select class='submit cat' name='category'><option class='category' value='' selected disabled style='display:none;'>Choose a category</option><option class='category' value='Advice'>Advice</option><option class='category' value='Design'>Design</option><option class='category' value='Funding'>Funding</option><option class='category' value='Growth'>Growth</option><option class='category' value='Product'>Product</option><option class='category' value='Sales'>Sales</option><option class='category' value='Strategy'>Strategy</option><option class='category' value='Team'>Team</option><option class='category' value='Tech'>Tech</option><option class='category' value='Users'>Users</option><option class='category' value='Venture'>Venture</option></select><input class='submit tags' name='tags' type='text' maxlength='75' placeholder='Separate tags with a space'><hr><input id='submit-btn' type='button' onclick='_gaq.push(['_trackEvent', 'Submit Post', 'Submit', 'Post Submitted']);' value='Submit'></div>");
     
     $('#close').click(function () {
+        $('#submit-display').remove();
+    });
+    $('#submit-panel').click(function () {
         $('#submit-display').remove();
     });
 
 }
 function submitPost() {
-
+	
 	var submitFields = [$('.title').val(), $('.link').val(), $('.image').val(), $('.desc').val(), $('.cat ').val(), $('.tags').val()];
     
     if (submitFields[0] === '' || submitFields[1] === '' || submitFields[3] === '' || submitFields[4] === '') {

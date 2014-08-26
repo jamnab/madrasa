9022907460fc14e41af425a1d2122eccd0d3d4ac
jamnab <jamesbmcnab@gmail.com>
2014-08-26 13:45:36 -0400

    tweaked link.js

--- js/link.js
+++ js/link.js
@@ -1,135 +1,134 @@
-$(function() {
+'use strict';
 
-  //Click event when particular link is pressed
-  $(document).ready( function(){
-
-      
-  });
-
-//Click event when particular link is pressed
-  $('.topic').click( function(){
-      
-      featureID = $(this).attr('id');
-	
-        loadFeatures();
-      opensesame();
-      //e.preventDefault();
-      
-  });
-   
+$(function () {
+    var featureID;
     
-     //Click event when Submit a link is pressed
-    $('#menu-btn').click( function() {
-            opensesame();
+    $(document).ready(function () {
+
+    });
+	
+	$('#header-panel').click(function () {
+		//$('html, body').animate({scrollTop: $('#main-panel').offset().top - 60}, 'slow', 'swing');
+	});
+    
+    $('.btn.menu').click(function () {
+    	opensesame();
+    });
+    
+    $('.btn.add').click(function () {
+    
+        submitAppear();
+        
+        $('#submit-btn').click(function () {
+            submitPost();
+        });
     });
       
-//Keypress event to see if enter was pressed in text input
-	$('#text').keydown(function(e){
-
-    	if(e.keyCode == 13){
-
-    		doSearch();
-
-			// Set it to an empty string
+	$('#text').keydown(function (e) {
+        if (e.keyCode === 13) {
+            doSearch();
             $('#text').val("");
-    	}
+        }
 	});
 
 });
 
+function loadInitial() {
+    var initialVal = true;
+    
+    $.ajax({
+        url: 'php/initial.php',
+		type: 'POST',
+		data: { 'initial' : initialVal },
+        beforeSend: function (data) {
+
+        },
+        success: function (data) {
+        	
+        	$.each(data.category, function (key, val) {
+        		$('#topic-list').append('<li><a class="topic" id="' + val + '" href="#"><h4>' + val + '</h4></a></li>');
+        	});
+        	
+        	$('.topic').click(function (featureID) {
+        		
+        		featureID = $(this).attr('id');
+        		loadFeatures(featureID);
+        	});
+
+            if (data.success) {
+
+                if (data.results.length > 0) {
+
+                    $('#title').append("Welcome, Here are some featured posts.");
+            
+                    $.each(data.results, function () {
+                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><div id='featured'><span>" + this.category + "</span></div><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div");
+                    });
+                    $('img').unveil(200);
+                } else {
+                    $('#title').append("Uh oh, something went wrong.");
+                }
+            } else {
+                alert(data.error);
+            }
+        }
+    });
+}
+
+function exploreClick() {
+	$('html, body').animate({scrollTop: $('#main-panel').offset().top - 60}, 'slow', 'swing');
+}
+
 function opensesame() {
     var rightval = $('#topic-list').css("right");
         
-        if(rightval == "-250px") {
-            $('#search-panel').animate({  
-                right: "250px"  
-              }, { duration: 225, queue: false });  
-              $('#main-panel').animate({  
-                right: "250px"  
-              }, { duration: 225, queue: false });
-                $('#topic-list').animate({  
-                right: "0px"  
-              }, { duration: 225, queue: false });
-        }
-        else {
-            $('#search-panel').animate({  
-                right: "0px"  
-              }, { duration: 300, queue: false });  
-              $('#main-panel').animate({  
-                right: "0px"  
-              }, { duration: 300, queue: false });
-                $('#topic-list').animate({  
-                right: "-250px"  
-              }, { duration: 300, queue: false });
-        }
-
-	$('#main-panel').click( function() {
-            opensesame();
-    	});
+    if (rightval === "-250px") {
+        $('#header-panel').animate({ right: "250px" }, { duration: 225, queue: false });
+        $('#banner').animate({ right: "250px" }, { duration: 225, queue: false });
+        $('#main-panel').animate({ right: "250px" }, { duration: 225, queue: false });
+        $('#topic-list').animate({ right: "0px" }, { duration: 225, queue: false });
+    } else {
+        $('#header-panel').animate({ right: "0px" }, { duration: 300, queue: false });
+        $('#banner').animate({ right: "0px" }, { duration: 300, queue: false });
+        $('#main-panel').animate({ right: "0px" }, { duration: 300, queue: false });
+        $('#topic-list').animate({ right: "-250px" }, { duration: 300, queue: false });
+    }
 }
 
-function loadFeatures() {
+function loadFeatures(featureID) {
 
-	$('#more').hide();
-
-  $.ajax({
-    
+    $.ajax({
         url: 'php/features.php',
 		type: 'POST',
-		data: { 
-            
-            'subject' : featureID
-              
-              },
-      beforeSend: function(data) {
-
-        $('#link-display').show();
-
-          //Display the featured posts
+		data: { 'subject' : featureID },
+        beforeSend: function (data) {
+        
             $('#text').val("");
-
-	window.scrollTo(0,0);
-      },
-    success: function(data) {
-
-      //Was everything successful, no errors in the PHP script
-      if (data.success) {
+            $('#banner').remove();
+            opensesame();
+        },
+        success: function (data) {
+            if (data.success) {
+                $('#link-display').empty();
+                $('#title').empty();
+                $('#text').val(featureID);
           
-            //Clear featured posts
-            $('#link-display').empty();
-
-            //Clear title
-            $('#title').empty();
-
-            //Display the featured posts
-            $('#text').val(featureID);
-          
-        //Check to see if there are any results to display
-        if(data.results.length > 0) {
+                if (data.results.length > 0) {
             
+                    $.each(data.results, function () {
+                        $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>" + this.tags + "</a></a></div");
+                    });
+                    $('img').unveil(200);
+                } else {
+                    $('banner').remove();
+                    $('#title').append("Uhhhh my bad...");
+                }
 
-        //Loop through each result and add it to the list
-          $.each(data.results, function() {
-
-        //Give the list element a rel with the data results ID incase we want to act on this later, like selecting from the list
-
-            $('#link-display').append("<div id='link-overlay'><a href='" + this.link + "' target='_blank'><img src='img/madrasa-ph.png' data-src='" + this.image + "'><h3>" + this.title + "</h3><p>" + this.description + "</p><hr><a id='tags' href='#'>"+ this.tags + "</a></a></div");
-          });
-            $("img").unveil(200);
-        } else {
-
-          //If there are no results, inform the user - add 'no-results' class so we can style it differently
-          $('#title').append("What did you do!?");
-
+            } else {
+                alert(data.error);
+            }
         }
-
-      } else {
-
-        //Display the error message
-        alert(data.error);
-      }
-    }
-  });
+    });
 }
 
 function doSearch() {
@@ -163,7 +162,7 @@ function doSearch() {
 				} else {
 					$('#link-display').append("<h1 id='title'>Sorry no results for <a href=''>" + searchText + "</a>. Here's some </h1>");
 					loadInitial();
-					$('#banner').remove();
+					
 				}
 
 			} else {

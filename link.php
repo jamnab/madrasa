1807cd43109328e0fe0b79086a65abf196a80dca
jamnab <jamesbmcnab@gmail.com>
2014-09-14 15:14:57 -0400

    Added clicky tracking code

--- link.php
+++ link.php
@@ -69,6 +69,10 @@ mixpanel.init(\"ac8ab27d18e0c61a278802634fc57cbd\");</script><!-- end Mixpanel -
 	<div id='banner-panel'></div>
 	<div id='main-panel'><div id='link-display'><h1 id='title'></h1></div></div>
     <div class='panel submission'></div>
+		<a title='Google Analytics Alternative' href='http://clicky.com/100771556'><img alt='Google Analytics Alternative' src='//static.getclicky.com/media/links/badge.gif' border='0' /></a>
+<script src='//static.getclicky.com/js' type='text/javascript'></script>
+<script type='text/javascript'>try{ clicky.init(100771556); }catch(e){}</script>
+<noscript><p><img alt='Clicky' width='1' height='1' src='//in.getclicky.com/100771556ns.gif' /></p></noscript>
 </body>
 <script ansyc src='http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js'></script>
     <script defer src='js/link.js'></script>
